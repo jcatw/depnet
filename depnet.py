1ac0dcf1495a40a077c19df2d145ba6188d38890
James Atwood <jatwood@cs.umass.edu>
2014-01-13 14:14:45 -0500

    now conditioning on features where only one value is ever observed

--- depnet.py
+++ depnet.py
@@ -5,28 +5,59 @@ import networkx as nx
 import multiprocessing as mp
 import itertools
 
-# this needs to sit outside the class for pool.map to work
+def all_same_value(vec):
+    #assumes that vec is binary
+    return vec.all() or not vec.any()
+
 def dn_fit_parallel(dn, n_proc):
+    """
+    Map dn_fit_parallel_worker over (dn, k) pairs
+    this is ugly but gets around the restriction that Pool.map funcs
+    be callable from the top of the module.
+
+    This needs to sit outside the class for pool.map to work.
+
+    """
     pool = mp.Pool(processes = n_proc)
-    # map dn_fit_parallel_worker over (dn, k) pairs
-    # this is ugly but gets around the restriction that Pool.map funcs
-    # be callable from the top of the module
+    
     cons = itertools.izip((dn for x in xrange(dn.n_features)),xrange(dn.n_features))
     conditional_models = pool.map(dn_fit_parallel_worker, cons)
     pool.close()
     return conditional_models
-                    
+
+def dn_fit(x,y):
+    """
+    If we only observe one y, x will completely seperate y,
+    and the logistic regression parameters will be meaningless.
+    We will condition on any such features, so return the value.
+    
+    Otherwise, fit a logistic regression, and return the model.
+    """
+    if (all_same_value(y)):
+        return y[0]
+    else:
+        model = LogisticRegression(penalty="l1")
+        model.fit(x, y)    
+        return model
+
+
 def dn_fit_parallel_worker(cons):
     dn, k = cons
-    model = LogisticRegression(penalty="l1")
-    model.fit(dn._get_x(k), dn._get_y(k))
-    return model
+    x = dn._get_x(k)
+    y = dn._get_y(k)
+    
+    return dn_fit(x, y)
+    
 
+# like dn_fit_parallel, this needs to sit outside the class for pool.map to work
 def dn_sample_parallel(dn, n_samples, n_chains, burn_in, burn_interval):
     pool = mp.Pool(processes = n_chains)  # one processor per chain
     samples_per_chain = np.zeros(n_chains, dtype="int64")
     for i in xrange(n_samples):
         samples_per_chain[i % n_chains] += 1
+    # map dn_sample_parallel_worker over (dn, n_samples_chain, burn_in, burn_interval) tuples
+    # this is ugly but gets around the restriction that Pool.map funcs
+    # be callable from the top of the module
     cons = itertools.izip((dn for x in xrange(n_chains)),
                           samples_per_chain,
                           (burn_in for x in xrange(n_chains)),
@@ -49,24 +80,45 @@ class depnet:
         self.X = X
         self.n_instances, self.n_features = self.X.shape
         self._col_indices = range(self.n_features)
+        self._conditioned_feature = [False] * self.n_features
+        self._conditioned_feature_val = {}
 
     def _get_x(self, k):
         return self.X[:,filter(lambda z: z != k, self._col_indices)]
     def _get_y(self, k):
         return self.X[:,k]
+    
 
     def fit(self):
         self.conditional_models = []
         for k in xrange(self.n_features):
-            model = LogisticRegression(penalty="l1")
-            model.fit(self._get_x(k), self._get_y(k))
+            x = self._get_x(k)
+            y = self._get_y(k)
+            fit_return = dn_fit(x,y)
+            if isinstance(fit_return,int) or isinstance(fit_return,float):
+                self.conditional_models.append(None)
+                self._conditioned_feature[k] = True
+                self._conditioned_feature_val[k] = fit_return
+            else:
+                self.conditional_models.append(fit_return)
 
     def fit_parallel(self, n_proc = 4):
         self.conditional_models = dn_fit_parallel(self, n_proc)
+        # track conditioned models
+        for k, model in enumerate(self.conditional_models):
+            if isinstance(model,int) or isinstance(model,float):
+                self.conditional_models[k] = None
+                self._conditioned_feature[k] = True
+                self._conditioned_feature_val[k] = model
 
     def _next_state(self, state, k):
-        conditioned_state = state[filter(lambda z: z != k, self._col_indices)]
-        return self.conditional_models[k].predict(conditioned_state)
+        # sub in conditioned feature
+        if self._conditioned_feature[k]:
+            return self._conditioned_feature_val[k]
+        # predict modeled feature
+        else:
+            conditioned_state = state[filter(lambda z: z != k, self._col_indices)]
+            return self.conditional_models[k].predict(conditioned_state)
 
     def sample(self, n_samples, burn_in=100, burn_interval=5):
         samples = np.zeros((n_samples, self.n_features))
@@ -76,15 +128,11 @@ class depnet:
         
         for i in xrange(burn_in):
             for k in xrange(self.n_features):
-                #conditioned_state = state[filter(lambda z: z != k, self._col_indices)]
-                #state[k] = self.conditional_models[k].predict(conditioned_state)
                 state[k] = self._next_state(state,k)
 
         for i in xrange(n_samples):
             for j in xrange(burn_interval):
                 for k in xrange(self.n_features):
-                    #conditioned_state = state[filter(lambda z: z != k, self._col_indices)]
-                    #state[k] = self.conditional_models[k].predict(conditioned_state)
                     state[k] = self._next_state(state,k)
             samples[i] = state
 
